2d86116b7d9cc440de582f62e6e353b264d853d3
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-01-17 09:43:17 +0000

    增加白名单

--- key system.lua
+++ key system.lua
@@ -3,6 +3,8 @@ if a=="tongguheren090325"then
 _G.bmd=true
 elseif a=="Chinarst3"then
 _G.bmd=true
+elseif a=="tongguheren0903253"
+_G.bmd=true
 end
 if _G.bmd==true then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhaodatt/script/main/TongScript"))()
