1b4a0f870732ba3d0b0f2c6ee72acd96706ae4ed
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-01-23 11:02:27 +0000

    1

--- TS/cdt ts.lua
+++ TS/cdt ts.lua
@@ -18,10 +18,10 @@ mt.__namecall =
 
 
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
-local window = DrRayLibrary:Load("TongScript-CDT", "Default")
-local tab = DrRayLibrary.newTab("车辆出售", "rbxassetid://16083820733")
+local window = DrRayLibrary:Load("TongScript-CDT", "rbxassetid://16042583196")
+local tab = DrRayLibrary.newTab("出售车辆", "rbxassetid://16084878908")
 
-tab.newToggle("自动卖车", "", false, function(state)
+tab.newToggle("自动卖车", "rbxassetid://16084730073", false, function(state)
     getfenv().Customer = (state and true or false)
         while getfenv().Customer do
             task.wait()
@@ -106,7 +106,7 @@ tab.newToggle("自动卖车", "", false, function(state)
         end
 end)
 
-tab.newToggle("自动送车", "", false, function(state)
+tab.newToggle("自动送车", "rbxassetid://16084903689", false, function(state)
     getfenv().deliver = (state and true or false)
         spawn(
             function()
@@ -181,9 +181,9 @@ end)
 
 
 
-local tab = DrRayLibrary.newTab("经销店", "")
+local tab = DrRayLibrary.newTab("经销店", "rbxassetid://16084916733")
 
-tab.newToggle("自动建店", "", false, function(state)
+tab.newToggle("自动建店", "rbxassetid://16084936359", false, function(state)
     getfenv().buyer = (state and true or false)
         while getfenv().buyer do
             task.wait()
@@ -210,9 +210,9 @@ tab.newToggle("自动建店", "", false, function(state)
         end
 end)
 
-local tab = DrRayLibrary.newTab("其他", "")
+local tab = DrRayLibrary.newTab("其他", "rbxassetid://16084948648")
 
-tab.newToggle("拦截弹窗", "", false, function(state)
+tab.newToggle("拦截弹窗", "694828233", false, function(state)
     getfenv().annoy = (state and true or false)
     if getfenv().annoy == true then
         -- annoying popup remover
--- cdt买车.lua
+++ cdt买车.lua
@@ -1,4 +1,4 @@
-local cars = {"Legends", "Indy", "AC", "MonsterJam", "BurgerCar","Avtoros","PlowTruck","BMW","Limo", "Porsche", "Bugatti", "McLaren", "Ferrari", "Koenigsegg", "Supra", "ShoppingCart", "Subaru", "Ford", "Nissan", "Polaris", "Dodge", "Honda", "Mazda", "Ariel", "Police", "Acura", "Hummer", "Audi", "AstonMartin", "MercedesBenz", "Pagani", "Lexus", "Apollo", "Pininfarina","Corvette", "Brabham", "Hennessey", "Tesla", "Jeep", "Murray"}
+local cars = {"Limo"}
 for i, car in pairs(cars) do
 coroutine.wrap(function()
 game.ReplicatedStorage.Remotes.Buy:FireServer(car)
