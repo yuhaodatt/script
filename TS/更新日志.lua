ed463b3e82a63f0d3f0f272724b35d742921eb00
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-01-25 20:02:51 +0800

    Update 更新日志.lua

--- TS/更新日志.lua
+++ TS/更新日志.lua
@@ -21,7 +21,7 @@ textLabel.TextColor3 = Color3.new(1, 1, 1) -- 文本颜色
 textLabel.Font = Enum.Font.SourceSansBold
 textLabel.FontSize = Enum.FontSize.Size24
 textLabel.TextWrapped = true
-textLabel.Text = "更新日志:增加时间和帧率显示功能/添加聊天翻译脚本"
+textLabel.Text = "更新日志:增加时间和帧率显示功能/添加聊天翻译脚本/添加某些反作弊游戏自动踢出"
 textLabel.Parent = containerFrame
 
 -- 创建一个ImageButton作为关闭按钮
