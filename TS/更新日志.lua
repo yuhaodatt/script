6e9e584e701a50886ac59ff024b80ecea01d19a0
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-02-20 15:46:49 +0000

    1

--- TS/更新日志.lua
+++ TS/更新日志.lua
@@ -3,7 +3,7 @@ local NotificationHolder =
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
 Notification:Notify(
-    {Title = "TS-更新日志", Description = "更改检测游戏逻辑/更改通知弹窗/即将更新唐县"},
+    {Title = "TS-更新日志", Description = "更改cc2/即将更新唐县"},
     {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 60, Type = "option"},
     {
         Image = "http://www.roblox.com/asset/?id=6023426923",
