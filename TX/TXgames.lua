d283c35b0a5192f30f5da21a4bdef7813232c396
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-05-16 14:09:36 +0000

    1

---
+++ TX/TXgames.lua
@@ -0,0 +1,38 @@
+local Games = {
+    [4618049391] = "TangCountyHebei.lua",
+}
+
+local repo = "https://raw.githubusercontent.com/yuhaodatt/script/main/游戏/"
+local scriptName = Games[game.PlaceId] or Games[1]
+local script = function() 
+    return game:HttpGet(repo..scriptName)
+end
+
+
+local function showNotification(gameName)
+    local NotificationHolder =
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+    local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+    Notification:Notify(
+        {Title = "Tong X", Description = "是否加载"..gameName.. "脚本"},
+        {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 999, Type = "option"},
+        {
+            Image = "http://www.roblox.com/asset/?id=6023426923",
+            ImageColor = Color3.fromRGB(255, 84, 84),
+            Callback = function(State)
+                if State then
+                    wait(1)
+                    loadstring(script())()
+                else
+                    print(tostring(State))
+                end
+            end
+        }
+    )
+end
+
+
+local detectedGameName = Games[game.PlaceId] or "universal.lua"
+detectedGameName = string.gsub(detectedGameName, ".lua", "")
+showNotification(detectedGameName)
---
+++ TX/TXwhitelist.lua
@@ -0,0 +1,68 @@
+local NotificationHolder =
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+local whitelist = {
+    "tongguheren090325",
+    "Tongdscsh",
+}
+
+local customerlist = {
+
+}
+
+local player = game.Players.LocalPlayer
+local playerName = player.Name
+local isInWhitelist = false
+local isInCustomer = false
+
+for _, allowedName in ipairs(whitelist) do
+    if playerName == allowedName then
+        isInWhitelist = true
+        break
+    end
+end
+
+for _, customerName in ipairs(customerlist) do
+    if playerName == customerName then
+        isInCustomer = true
+        break
+    end
+end
+
+if isInCustomer then
+    Notification:Notify(
+    {Title = "TX-BETA", Description = "检测到顾客名单,正在加载"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+end
+
+if isInWhitelist then
+    Notification:Notify(
+    {Title = "TX-BETA", Description = "检测admin名单,正在加载"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+    )
+end
+
+if isInWhitelist or isInCustomer then
+
+    game.Players.PlayerAdded:Connect(
+        function(player)
+            local owner = player.Name
+            if owner == "tongguheren090325" or owner == "Tongdscsh" then
+                Notification:Notify(
+                {Title = "TX", Description = "TX作者-Tong进入了服务器"},
+                {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+                {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+                )
+            end
+        end
+    )
+    print("Hello World")
+    wait(0.5)
+    print("Hello,World")
+else
+    game.Players.LocalPlayer:Kick("未检测到白名单")
+end
