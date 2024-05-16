c16e0edf588372bc646caafb70bc344346e91a58
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-05-16 14:24:47 +0000

    1

--- TX/tx.lua
+++ TX/tx.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhaodatt/script/main/TX/TXwhitelist.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhaodatt/script/main/TX/TXwhitelist.lua"))()
