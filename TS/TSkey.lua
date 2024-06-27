f0c0750102a83fe858716e4c8a3c181e778c8523
TongScript <99662857+yuhaodatt@users.noreply.github.com>
2024-06-27 16:10:42 +0800

    Update TSkey.lua

--- TS/TSkey.lua
+++ TS/TSkey.lua
@@ -11,8 +11,7 @@ local whitelist = {
 
 local customerlist = {
     "Lucenzous",--cdt 1.6已付款
-    "xxbudjf",--de 20m
-    "awbebheh",--cdt 50m
+    "dzsdnjsa",--cdt赛季
 }
 
 local player = game.Players.LocalPlayer
